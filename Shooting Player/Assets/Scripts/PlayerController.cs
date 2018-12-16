47f3152a535ac82a9d6f470aa5d52e0bc1837d06
undermoongames <45921256+undermoongames@users.noreply.github.com>
2018-12-16 11:44:14 -0800

    First Input
    I Do First Input Of Project To Git Hub! Next Is Yours...

---
+++ Shooting Player/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime ;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x*300.0f, 0);
+		transform.Translate(x*3.0f, 0, 0);
+		transform.Translate(0, 0, z);
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
+
+	}
+
+
+	void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 10;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
+}
